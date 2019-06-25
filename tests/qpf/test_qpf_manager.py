52e92dd5d0d3701ab3b4168871fc5a7d15025468
Mark Jensen <mark.jensen@pnnl.gov>
2019-06-25 15:30:55 -0700

    Update to the qpf_manager

--- tests/qpf/test_qpf_manager.py
+++ tests/qpf/test_qpf_manager.py
@@ -15,18 +15,22 @@ class TestQpfManager:
         qpf = QpfManager(testing=True)
         ftp = Mock()
         with patch('vikingapp.qpf.qpf_manager.open', mock_open()) as m_file:
-            qpf.download_file_locally(ftp, m_filepath)
+            qpf._download_file_locally(ftp, m_filepath)
             m_file.assert_called_once_with(Path(m_filepath), 'wb')
             ftp.retrbinary.assert_called_once()
 
     @patch('vikingapp.qpf.qpf_manager.FTP')
     def test_download_2p5km_qpf_grib_files(self, mock_ftp):
         qpf = QpfManager(testing=True)
-        hour = '00'
-        period = '072'
+        periods = ['006', '012', '018']
 
         mock_files = MagicMock()
-        mock_files.return_value = ['p06m_%s%sf%s.grb' % (qpf.today, qpf.hour, period), 'test2.grb']
+        
+        tst_files = ['test2.grb']
+        for period in periods:
+            tst_files.append('p06m_%s%sf%s.grb' % (qpf.today, qpf.hour, period))
+        mock_files.return_value = tst_files
+
         mock_ftp.return_value = Mock(
             __enter__=mock_ftp,
             __exit__=Mock(),
@@ -35,21 +39,26 @@ class TestQpfManager:
             nlst=mock_files
         )
 
-        qpf.download_2p5km_data(period, 'test.grb')
+        qpf._download_qpf_data(periods)
 
         m_ftp = mock_ftp.return_value
         m_ftp.login.assert_called_once()
         m_ftp.cwd.assert_called_once_with('/2p5km_qpf')
-        assert len(qpf.qpf_files) == 1
+        assert len(qpf.qpf_files) == 3
 
     @patch('vikingapp.qpf.qpf_manager.FTP')
     def test_download_5km_qpf_grib_files(self, mock_ftp):
         qpf = QpfManager(testing=True)
-        hour = '00'
-        period = '072'
+        qpf.res = '5km_qpf'
+        periods = ['012', '018']
 
         mock_files = MagicMock()
-        mock_files.return_value = ['p06m_%s%sf%s.grb' % (qpf.today, qpf.hour, period), 'test2.grb', 'p06m_%s%sf%s.grb' % (qpf.today, qpf.hour, period)]
+        
+        tst_files = ['test1.grb', 'test2.grb']
+        for period in periods:
+            tst_files.append('p06m_%s%sf%s.grb' % (qpf.today, qpf.hour, period))
+        mock_files.return_value = tst_files
+
         mock_ftp.return_value = Mock(
             __enter__=mock_ftp,
             __exit__=Mock(),
@@ -57,18 +66,42 @@ class TestQpfManager:
             login=Mock(),
             nlst=mock_files
         )
-        
-        qpf.download_5km_data(period, 'test.grb')
+
+        qpf._download_qpf_data(periods)
+
         m_ftp = mock_ftp.return_value
         m_ftp.login.assert_called_once()
         m_ftp.cwd.assert_called_once_with('/5km_qpf')
         assert len(qpf.qpf_files) == 2
 
-    @patch.object(QpfManager, 'download_2p5km_data')
-    def test_get_multiple_periods(self, mock_download):
+    # @patch.object(QpfManager, 'download_qpf_data')
+    def test_get_by_periods(self):
         qpf = QpfManager(testing=True)
-        periods = ['06', '12', '18']
-        qpf.get_qpf_by_periods(periods)
+        periods = ['006', '012', '018']
+        filenames = qpf._get_qpf_by_periods(periods)
+        for i in range(0,3):
+            assert periods[i] in filenames[i]
 
-        assert mock_download.call_count == 3
+    @patch('vikingapp.qpf.qpf_manager.call')
+    def test_prep_grib_to_tiff(self, mock_call):
+        qpf = QpfManager(testing=True)
+        qpf._prep_grib_to_tiff('test.grb')
+        assert mock_call.call_count == 2
 
+    @patch('vikingapp.qpf.qpf_manager.call')
+    def test_clip_to_bb(self, mock_call):
+        qpf = QpfManager(testing=True)
+        bb = {  
+                'huc': '001',
+                'minx': 0,
+                'miny': 0,
+                'maxx': 10,
+                'maxy': 15
+            }
+        qpf._clip_to_bb(bb, '0.000833333334000', 'test.tif')
+        mock_call.assert_called_once()
+
+    @patch('vikingapp.qpf.qpf_manager.rio')
+    def test_convert_in_to_mm(self, mock_rasterio):
+        qpf = QpfManager(testing=True)
+        qpf._convert_in_to_mm('test.tif')
--- vikingapp/qpf/qpf_manager.py
+++ vikingapp/qpf/qpf_manager.py
@@ -2,50 +2,113 @@ import time
 from pathlib import Path
 from ftplib import FTP
 import pdb
+from subprocess import call
+import rasterio as rio
 
 class QpfManager:
-    DEFAULTS: dict = {'today': None, 'hour': '00', 'output_path': '/tmp/qpf'}
+    DEFAULTS: dict = {
+                'today': None,
+                'hour': '00',
+                'output_path': '/tmp/qpf',
+                'res': '2p5km_qpf',
+                'periods': ['006', '012', '018', '024', '030', '036', '042', '048', '054', '060', '066', '072']
+            }
+
     def __init__(self, qpf_meta=DEFAULTS, testing=False):
         self.today = time.strftime('%Y%m%d') if qpf_meta['today'] is None else qpf_meta['today']
         self.hour = qpf_meta['hour']
         self.qpf_files = []
         self.output_path = Path(qpf_meta['output_path'])
+        self.res = qpf_meta['res']
         if not self.output_path.exists() and not testing:
             self.output_path.mkdir()
 
-    def download_file_locally(self, ftp, output_file):
+    def _download_file_locally(self, ftp, output_file):
         with open(Path(self.output_path, output_file), 'wb') as grb_file:
             ftp.retrbinary("RETR " + output_file, grb_file.write)
 
-    def download_2p5km_data(self, period, output_file):
-        """ Need to get the list of hourly forecasts for the forecast date for the 2.5km """
+    def _download_qpf_data(self, periods=DEFAULTS['periods']):
+        """ Need to get the list of hourly forecasts for the forecast date for the 2.5km/5km options """
         with FTP('ftp.wpc.ncep.noaa.gov') as ftp:
             ftp.login()
-            ftp.cwd('/2p5km_qpf')
-            self.qpf_files = [x for x in ftp.nlst() if 'p06m_%s%sf%s' % (self.today, self.hour, period) in x]
+
+            if '2p5km_qpf' in self.res:
+                ftp.cwd('/2p5km_qpf')
+            else:
+                ftp.cwd('/5km_qpf')
+
+            ftp_files = ftp.nlst()
+            period_files = self._get_qpf_by_periods(periods)
+            self.qpf_files = [x for x in ftp_files if x in period_files]
+
             for q_file in self.qpf_files:
-                self.download_file_locally(ftp, q_file)
+                self._download_file_locally(ftp, q_file)
 
-    def download_5km_data(self, period, output_file):
-        """ Need to get the list of hourly forecasts for the forecast date for the 5km """
-        with FTP('ftp.wpc.ncep.noaa.gov') as ftp:
-            ftp.login()
-            ftp.cwd('/5km_qpf')
-            self.qpf_files = [x for x in ftp.nlst() if 'p06m_%s%sf%s' % (self.today, self.hour, period) in x]
-            # with open(output_file, 'wb') as grb_file:
-            #     ftp.retbinary("RETR " + grib, grb_file.write)
-
-    def get_qpf_by_periods(self, periods):
+    def _get_qpf_by_periods(self, periods):
+        period_files = []
         for period in periods:
-            self.download_2p5km_data(self.hour, period)
+            period_files.append('p06m_%s%sf%s.grb' % (self.today, self.hour, period))
+        return period_files
+
+    # TODO: Need to figure file paths for these
+    def _prep_grib_to_tiff(self, grib_file):
+        tmp_path = Path(self.output_path, 'tmp')
+        fin_path = Path(self.output_path, 'final')
+
+        if not tmp_path.exists():
+            tmp_path.mkdir()
+        if not fin_path.exists():
+            fin_path.mkdir()
+
+        tif_name = grib_file.split('.')[0] + '.tif'
+
+        tmp_file = Path(tmp_path, tif_name)
+        fin_file = Path(fin_path, tif_name)
+        try:
+            call(['gdal_translate', '-of','GTiff', Path(self.output_path, grib_file), tmp_file])
+        except Exception as e:
+            print('GDAL_TRANSLATE (grib_to_tiff): ' + e)
+
+        try:
+            call(['gdalwarp', '-t_srs', 'EPSG:4326', '-dstnodata','0', tmp_file, fin_file])
+        except Exception as e:
+            print('GDALWARP (grib_to_tiff): ' + e)
+
+    def _clip_to_bb(self, bounding_box, res, filename):
+        in_file = Path(self.output_path, 'final', filename)
+        fin_path = Path(self.output_path, 'output')
+        if not fin_path.exists():
+            fin_path.mkdir()
+        fin_file = Path(fin_path, filename)
+        call(['gdalwarp', '-te', str(bounding_box['minx']), str(bounding_box['miny']), str(bounding_box['maxx']), str(bounding_box['maxy']), '-tr', res, res, str(in_file), str(fin_file)])
+
+
+    # TODO: rasterio convert to mm/hr (this should probably be abstracted because QPE will probably use this as well)
+    def _convert_in_to_mm(self, tif_file):
+        with rio.open(tif_file) as src:
+            data = src.read(1)
+            data = data * 25.4
 
     # def __del__(self):
     #     self.output_path.rmdir()
     #     print('destructor called...')
-        
 
 if __name__ == '__main__':
     qpf = QpfManager()
-    qpf.download_2p5km_data('072', 'qpf-trial.grb')
+    qpf.output_path = Path('/Users/jens402/qfiles')
 
+    if not qpf.output_path.exists():
+        qpf.output_path.mkdir()
 
+    qpf._download_qpf_data()
+    grib_file = 'p06m_2019062500f012.grb'
+    tif_file = 'p06m_2019062500f012.tif'
+
+    qpf._prep_grib_to_tiff(grib_file)
+    bb = {
+            'minx': -90.047,
+            'miny': 28.972,
+            'maxx': -82.635,
+            'maxy': 32.922
+        }
+    qpf._clip_to_bb(bb, '0.000833333334000', tif_file)
